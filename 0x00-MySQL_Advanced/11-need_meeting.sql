23fde1d373488db723578cce5687bb093f22ac4b
root <pitsomnk@gmail.com>
2024-10-21 08:42:52 +0000

    anotherone

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
-- a SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
+-- a SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS 
